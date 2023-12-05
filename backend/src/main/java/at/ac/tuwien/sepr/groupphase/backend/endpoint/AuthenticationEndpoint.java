eb30a38cc8f5fad95ad4be5f6485582f28cb9d21
Alexander-P <pollek@gmx.at>
2023-12-05 22:44:48 +0100

    feat #13: Implemented Get and Update UserSettings in Backend Layers

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/AuthenticationEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/AuthenticationEndpoint.java
@@ -5,21 +5,27 @@ import at.ac.tuwien.sepr.groupphase.backend.auth.PasswordEncoder;
 import at.ac.tuwien.sepr.groupphase.backend.auth.SessionManager;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.LoginDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserRegisterDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserSettingsDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserUpdateDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.UserMapper;
 import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepr.groupphase.backend.exception.AuthenticationException;
-import at.ac.tuwien.sepr.groupphase.backend.service.UserService;
 import at.ac.tuwien.sepr.groupphase.backend.exception.UserNotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
 import java.lang.invoke.MethodHandles;
 
 /**
@@ -32,18 +38,20 @@ public class AuthenticationEndpoint {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public AuthenticationEndpoint(UserService userService) {
+    public AuthenticationEndpoint(UserService userService, UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     /**
      * Logs in a user, using data from {@code userLoginDto}.
      *
-     * @author Marc Putz
      * @param userLoginDto login data from the client.
      * @return a ResponseEntity to send back to the client, containing the JWT authentication token.
      * @throws AuthenticationException if the login data could not be matched to a user or user could not be authenticated.
+     * @author Marc Putz
      */
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto userLoginDto) throws AuthenticationException {
@@ -97,11 +105,80 @@ public class AuthenticationEndpoint {
     }
 
     /**
+     * Updates user profile data.
+     * Requires a valid JWT in the request header for authentication.
+     *
+     * @param userUpdateDto DTO containing updated user information.
+     * @param headers       HTTP headers from the request, containing the JWT token.
+     * @return ResponseEntity indicating the outcome of the operation.
+     */
+    @PutMapping("/settings")
+    public ResponseEntity<UserSettingsDto> updateSettings(@RequestBody UserUpdateDto userUpdateDto,
+                                                          @RequestHeader HttpHeaders headers) {
+        LOGGER.trace("update({})", userUpdateDto);
+        try {
+            // retrieve token from authorization header
+            String authToken = headers.getFirst("authorization");
+
+            if (authToken == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            Long currentUserId = SessionManager.getInstance().getUserFromAuthToken(authToken);
+            if (currentUserId == null) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            // Perform the update operation for the authenticated user
+            ApplicationUser updatedUser = userService.update(userUpdateDto, currentUserId);
+            UserSettingsDto userSettingsDto = userMapper.toUserSettingsDto(updatedUser);
+
+            return ResponseEntity.ok(userSettingsDto);
+
+        } catch (Exception e) {
+            // Handle exceptions, such as authorization failure or other errors
+            LOGGER.error("Error in update: ", e);
+            //TODO Correct Exception Handling
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error during update: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/settings")
+    public ResponseEntity<UserSettingsDto> getSettings(@RequestHeader HttpHeaders headers) {
+        LOGGER.trace("getSettings()");
+        try {
+            // retrieve token from authorization header
+            String authToken = headers.getFirst("authorization");
+
+            if (authToken == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            Long currentUserId = SessionManager.getInstance().getUserFromAuthToken(authToken);
+            if (currentUserId == null) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            // Fetch user details and convert to DTO
+            ApplicationUser currentUser = userService.getUserById(currentUserId);
+            UserSettingsDto userSettingsDto = userMapper.toUserSettingsDto(currentUser);
+
+            return ResponseEntity.ok(userSettingsDto);
+
+        } catch (Exception e) {
+            // Handle exceptions, such as user not found, authorization failure, or other errors
+            LOGGER.error("Error in getSettings: ", e);
+            //TODO Correct Exception Handling
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving user settings: " + e.getMessage());
+        }
+    }
+
+    /**
      * Logs out a user, using the JWT authentication token in the HTTP header
      *
-     * @author Marc Putz
      * @param headers header of the HTTP request
      * @return a ResponseEntity to send back to the client, containing a boolean value indicating if the logout operation was successful.
+     * @author Marc Putz
      */
     @PostMapping("/logout")
     public ResponseEntity<Boolean> logout(@RequestHeader HttpHeaders headers) {
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserService.java
@@ -1,10 +1,11 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserRegisterDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserUpdateDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
-import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.UserNotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 
 /**
  * UserService interface to handle data storage for users.
@@ -29,7 +30,30 @@ public interface UserService {
      */
     ApplicationUser getUserByNickname(String nickname) throws UserNotFoundException;
 
-    ApplicationUser update(ApplicationUser applicationUser) throws UserNotFoundException, ValidationException, ConflictException;
+    /**
+     * Retrieves an ApplicationUser by their ID.
+     *
+     * @param userId the ID of the user
+     * @return the ApplicationUser entity
+     * @throws UserNotFoundException if no user is found with the provided ID
+     */
+    ApplicationUser getUserById(Long userId) throws UserNotFoundException;
+
+    /**
+     * Updates an ApplicationUser with the details provided in UserUpdateDto.
+     * Validates the existence of the user specified by currentUserId, checks for email and nickname uniqueness,
+     * and updates user data in the database. This method ensures that only the user corresponding to currentUserId
+     * can update their own profile.
+     *
+     * @param userUpdateDto The DTO containing updated information for the user, including ID, email, nickname, and encoded password.
+     * @param currentUserId The ID of the user currently logged in and requesting the update. This is used to ensure that
+     *                      only the authenticated user can update their own information.
+     * @return ApplicationUser The updated user object after successful persistence.
+     * @throws UserNotFoundException If no user is found with the provided currentUserId.
+     * @throws ValidationException   If input validation fails.
+     * @throws ConflictException     If email or nickname conflicts are found with existing users.
+     */
+    ApplicationUser update(UserUpdateDto userUpdateDto, Long currentUserId) throws UserNotFoundException, ValidationException, ConflictException;
 
     ApplicationUser create(UserRegisterDto registerDto);
 }
