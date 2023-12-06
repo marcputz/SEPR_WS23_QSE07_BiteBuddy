fb15f724de35a5b0cf4d990fc5398596f1647870
Giulia Gallico <e1325712@student.tuwien.ac.at>
2023-12-06 07:40:38 +0100

    Adding bootstraping landing page at start and changes on style layouts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {AuthGuard} from './guards/auth.guard';
 import {MessageComponent} from './components/message/message.component';
+import {LandingLayoutComponent} from "./layouts/landing-layout/landing-layout.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  //{path: '', component: HomeComponent},
+  {path: '', component: LandingLayoutComponent},
   {path: 'login', component: LoginComponent},
   {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent}
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,10 @@ import {LoginComponent} from './components/login/login.component';
 import {MessageComponent} from './components/message/message.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {httpInterceptorProviders} from './interceptors';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LandingLayoutComponent } from './layouts/landing-layout/landing-layout.component';
+import { LandingHeaderComponent } from './components/landing-header/landing-header.component';
+import { LandingFooterComponent } from './components/landing-footer/landing-footer.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +25,10 @@ import {httpInterceptorProviders} from './interceptors';
     HomeComponent,
     LoginComponent,
     MessageComponent,
+    LandingPageComponent,
+    LandingLayoutComponent,
+    LandingHeaderComponent,
+    LandingFooterComponent,
   ],
   imports: [
     BrowserModule,
