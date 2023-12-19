c6ad576251ee64c7217edb95238d42feb96cee21
Marc Putz <e12024709@student.tuwien.ac.at>
2023-12-19 09:16:29 +0100

    feat #29: removed template test classes in backend

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/basetest/TestData.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/basetest/TestData.java
@@ -1,19 +1,5 @@
 package at.ac.tuwien.sepr.groupphase.backend.basetest;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 public interface TestData {
-
-    Long ID = 1L;
-    String TEST_NEWS_TITLE = "Title";
-    String TEST_NEWS_SUMMARY = "Summary";
-    String TEST_NEWS_TEXT = "TestMessageText";
-    LocalDateTime TEST_NEWS_PUBLISHED_AT =
-        LocalDateTime.of(2019, 11, 13, 12, 15, 0, 0);
-
-    String BASE_URI = "/api/v1";
-    String MESSAGE_BASE_URI = BASE_URI + "/messages";
 
 }
