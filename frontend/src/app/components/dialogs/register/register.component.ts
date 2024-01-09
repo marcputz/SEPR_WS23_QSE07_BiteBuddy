01b1eabefcb1f50c81945b31ad4fbd1591c623b5
Alexander-P <pollek@gmx.at>
2024-01-09 14:16:07 +0100

    fix #13: Added username/nickname minLength of 3 like in Backend

--- frontend/src/app/components/dialogs/register/register.component.ts
+++ frontend/src/app/components/dialogs/register/register.component.ts
@@ -12,7 +12,7 @@ import {ToastrService} from "ngx-toastr";
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
 
   registerForm: UntypedFormGroup;
   // After first submission attempt, form validation will start
@@ -29,7 +29,7 @@ export class RegisterComponent implements OnInit{
               private router: Router,
               private notification: ToastrService) {
     this.registerForm = this.formBuilder.group({
-      username: ['', [Validators.required]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       password2: ['', [Validators.required, Validators.minLength(8)]]
@@ -43,9 +43,9 @@ export class RegisterComponent implements OnInit{
   registerUser() {
     this.submitted = true;
     if (this.registerForm.valid && this.registerForm.controls.password.value == this.registerForm.controls.password2.value) {
-      const name: string = <string> this.registerForm.controls.username.value;
-      const email: string = <string> this.registerForm.controls.email.value;
-      const password: string = <string> this.registerForm.controls.password.value;
+      const name: string = <string>this.registerForm.controls.username.value;
+      const email: string = <string>this.registerForm.controls.email.value;
+      const password: string = <string>this.registerForm.controls.password.value;
       const encodedPassword = this.passwordEncoder.encodePassword(password);
 
       const registerDto: RegisterDto = new RegisterDto(email, name, encodedPassword);
@@ -66,7 +66,7 @@ export class RegisterComponent implements OnInit{
    * @param authRequest authentication data from the user login form
    */
   authenticateUser(authRequest: RegisterDto) {
-    console.log('Try to register user: ' + authRequest.name + ' with email: ' + authRequest.email + ' with encoded password ' + authRequest.passwordEncoded );
+    console.log('Try to register user: ' + authRequest.name + ' with email: ' + authRequest.email + ' with encoded password ' + authRequest.passwordEncoded);
     this.authService.registerUser(authRequest).subscribe({
       next: (data) => {
         console.log('Successfully registered user: ' + authRequest.email);
@@ -108,7 +108,9 @@ export class RegisterComponent implements OnInit{
               this.registerForm.controls['username'].setErrors({userAlreadyExists: true});
               break;
             }
-          default: this.notification.error("Error while logging in, try again later."); break;
+          default:
+            this.notification.error("Error while logging in, try again later.");
+            break;
         }
       }
     });
