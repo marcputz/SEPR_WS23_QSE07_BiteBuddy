0a0db44ac980519961210337fd60673e8b469c03
Alexander-P <pollek@gmx.at>
2024-01-25 10:44:09 +0100

    style #29: fixed checkstyle errors

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ProfileEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ProfileEndpoint.java
@@ -13,8 +13,14 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
 
@@ -34,7 +40,7 @@ public class ProfileEndpoint {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "201", description = "Created"),
         @ApiResponse(responseCode = "422", description = "Unable to process the data, because contains invalid data"),
-        @ApiResponse(responseCode = "400", description = "Illegal arguments in the request body") })
+        @ApiResponse(responseCode = "400", description = "Illegal arguments in the request body")})
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ProfileDto post(@Valid @RequestBody ProfileDto toCreateProfile) throws ValidationException {
@@ -52,12 +58,12 @@ public class ProfileEndpoint {
         profileService.rateRecipe(recipeRatingDto);
     }
 
-    @GetMapping("/rating/{UserId}")
-    public RecipeRatingListsDto get(@PathVariable long UserId) throws NotFoundException {
+    @GetMapping("/rating/{userId}")
+    public RecipeRatingListsDto get(@PathVariable long userId) throws NotFoundException {
         LOGGER.info("Received Get request on {}", BASE_PATH);
-        LOGGER.debug("Request body for Get:\n{}", UserId);
+        LOGGER.debug("Request body for Get:\n{}", userId);
 
-        return profileService.getRatingLists(UserId);
+        return profileService.getRatingLists(userId);
     }
 
 }
